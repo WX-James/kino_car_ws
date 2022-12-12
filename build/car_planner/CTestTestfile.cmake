2339f8b9909df651efddf3eb65c84af72f2f3a23
WX-James <2303069947@qq.com>
2022-12-12 13:11:34 +0800

    remove build & devel

--- build/car_planner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Desktop/kino_car_ws/src/car_planner
-# Build directory: /home/pi/Desktop/kino_car_ws/build/car_planner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/map_generator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Desktop/kino_car_ws/src/map_generator
-# Build directory: /home/pi/Desktop/kino_car_ws/build/map_generator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
